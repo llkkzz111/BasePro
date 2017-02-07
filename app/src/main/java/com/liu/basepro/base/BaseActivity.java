4d9ab84ed9874a7c4116e11928d761932597d34f
liuz <llkkzz111@126.com>
2017-02-07 18:24:01 +0800

    1、添加简单的透明titlebar显示

--- app/src/main/java/com/liu/basepro/base/BaseActivity.java
+++ app/src/main/java/com/liu/basepro/base/BaseActivity.java
@@ -1,8 +1,11 @@
 package com.liu.basepro.base;
 
-import android.app.Activity;
 import android.content.pm.ActivityInfo;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 
 import com.liu.basepro.AppManager;
@@ -14,7 +17,7 @@ import butterknife.ButterKnife;
  * Created by liuzhao on 17/2/6.
  */
 
-public abstract class BaseActivity<T extends IPresenter> extends Activity {
+public abstract class BaseActivity<T extends IPresenter> extends FragmentActivity {
 
     public T mPresenter;
 
@@ -34,6 +37,13 @@ public abstract class BaseActivity<T extends IPresenter> extends Activity {
         AppManager.getAppManager().addActivity(this);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        ViewGroup contentFrameLayout = (ViewGroup) findViewById(Window.ID_ANDROID_CONTENT);
+        View parentView = contentFrameLayout.getChildAt(0);
+        if (parentView != null && Build.VERSION.SDK_INT >= 14) {
+            parentView.setFitsSystemWindows(true);
+        }
+
     }
 
     private void initTheme() {
--- app/src/main/java/com/liu/basepro/ui/main/MainActivity.java
+++ app/src/main/java/com/liu/basepro/ui/main/MainActivity.java
@@ -1,24 +1,26 @@
 package com.liu.basepro.ui.main;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 
 import com.liu.basepro.R;
+import com.liu.basepro.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
+    protected int getLayoutId() {
+        return R.layout.main_layout;
+    }
+
+    @Override
+    protected void initEventAndData() {
         setNewRootFragment();
     }
 
 
     private void setNewRootFragment() {
-        Fragment fragment = null;
-        fragment = new MainFragment();
+        Fragment fragment = new MainFragment();
+
         getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
     }
 }
